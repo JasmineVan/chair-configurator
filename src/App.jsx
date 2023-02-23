085b19a45348b7d322f2ff7df8bfacb4a1bd111f
TrinhThuong2001 <88668726+JasmineVan@users.noreply.github.com>
2023-02-23 23:58:46 +0700

    Section 2: Controller - add orbit controller to the scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Canvas } from '@react-three/fiber'
+import { Color } from 'three'
 import './App.css'
 import Experience from './components/Experience'
 
 function App() {
   return (
     <div className="App">
-        <Canvas>
-          <Experience/>
-        </Canvas>
+      <Canvas>
+        <color attach="background" args={["#E8D5C4"]} />
+        <Experience />
+      </Canvas>
     </div>
   )
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,17 @@
+import { PresentationControls, Stage } from "@react-three/drei";
 import { BoxGeometry, Mesh } from "three";
 
 const Experience = () => {
     return (
-        <mesh>
-            <boxGeometry/>
-            <meshStandardMaterial/>
-        </mesh>
+        <PresentationControls speed={1.5} global zoom={0.7} polar={[-0.1, Math.PI / 4]}>
+            <Stage environment={"city"} intensity={0.6} contactShadow={false}>
+                <mesh>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial />
+                </mesh>
+            </Stage>
+        </PresentationControls>
+
     );
 };
 
